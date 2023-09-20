b73e6ee7927af04a4d9f0f5beb8bf79ccecbf955
SAURABH <sgupta8mz@gmail.com>
2023-09-20 20:52:48 +0530

    Day 3 commit

---
+++ Day-3/script.js
@@ -0,0 +1,44 @@
+
+let items = document.querySelectorAll('li');
+console.log(items);
+
+
+let ni = document.createElement("li");
+ni.innerText="Hawkeye";
+
+
+document.getElementById('list-items').appendChild(ni);
+
+items = document.querySelectorAll('li');
+console.log(items);
+
+
+
+
+
+
+
+
+
+
+
+
+// work on htmlcollection logic  
+// children 
+// parentElement 
+//nextElementSibling
+//previousElementSibling
+
+// works with nodelist logic
+// childNodes 
+// parentNode
+//nextSibling
+//previousSibling
+
+
+
+
+// text node 
+// comment node 
+// element 
+
